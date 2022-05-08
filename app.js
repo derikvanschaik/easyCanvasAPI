c3b103d0451cd9f2bc28b42c1c00f7563aef16ec
Derik van Schaik <derikvanschaik@gmail.com>
2022-05-08 13:26:09 -0700

    example with multiple textboxes

--- app.js
+++ app.js
@@ -1,26 +1,54 @@
-import {Box} from './easyCanvas/shapes/Box.js' 
+import {TextBox} from './easyCanvas/shapes/TextBox.js' 
 import {CanvasStage} from './easyCanvas/canvas/CanvasStage.js' 
 
 function main(){ 
     const canvas = document.querySelector("canvas"); 
     const ctx = canvas.getContext("2d");
+    canvas.height = window.innerHeight; 
+    canvas.width = window.innerWidth; 
 
-    const stage = new CanvasStage(canvas); 
+    const stage = new CanvasStage(canvas, ctx);
 
-    // args = x, y, width, height, ctx, draggable (boolean) 
-    const b1 = new Box(50, 50, 100, 100, ctx, false); 
-    const b2 = new Box(150, 190, 100, 100, ctx, true); 
+    // args = x, y, width, height, ctx, draggable (boolean), text
+    const t = new TextBox(50, 50, 100, 100, ctx, true, "Hello World"); 
+    const t2 = new TextBox(150, 190, 100, 100, ctx, true, "Example\nhaha isn't this cool?"); 
+    const t3 = new TextBox(400, 100, 100, 200, ctx, true, "Box over here!"); 
+    
+    // draw the text boxes onto the canvas using their methods 
+    t.drawTextBox(); 
+    t2.drawTextBox();
+    t3.drawTextBox(); 
 
-    b1.drawBox();
-    b2.drawBox();
+    let selected = null; 
 
-    b1.onHover(() => console.log(`mouse${b1.isHoveredOn?'enter':'leave'}`), []);  
+    // add click listeners to each textbox (as if they were html elements!)
+    [t, t2, t3].forEach(box =>{
 
-    b2.onDrag(()=>console.log("dragging b2"), []);  
-    b2.onClick(()=> console.log("clicked b2!"), []);  
+        box.onClick(function(){ 
+            if(selected === null){
+                // turns box editing on 
+                selected = box;
+                box.toggleEditing(); 
+                return; 
+            }
+            if(selected !== box){
+                // turns last box editing off 
+                selected.toggleEditing(); 
+                selected = box;
+                box.toggleEditing(); 
+                return; 
+            }
+            // selected is box, should turn editing off and reset selected 
+            selected.toggleEditing(); 
+            selected = null;  
+        }, []); 
+    })
 
-    stage.addObject(b1);
-    stage.addObject(b2);  
+    // add the objects to the stage, this allows the api to keep track of the objects 
+    // and handle their event listeners and fire them. 
+    stage.addObject(t);
+    stage.addObject(t2);
+    stage.addObject(t3); 
 
 }
 window.addEventListener("load", main);   
