a05554a6bf9c4ffa0377cf8fcb0f54526b0f9a7d
Derik van Schaik <derikvanschaik@gmail.com>
2022-05-07 23:01:06 -0700

    can now trigger edit and stop edit into textbox, need to test multiple

--- easyCanvas/shapes/TextBox.js
+++ easyCanvas/shapes/TextBox.js
@@ -2,12 +2,17 @@ import {Box} from './Box.js';
 import {getFontDimensions} from '../utils/getFontDimensions.js'; 
 
 class TextBox extends Box{ 
-    constructor(x, y, w, h, ctx, draggable, text){  
+    constructor(x, y, w, h, ctx, draggable, text){ 
         super(x, y, h, w, ctx, draggable);  
         this.text = text;
         this.name = "textbox";
+        this.editing = false;
+        // we need this reference here for the window listener 
+        // if not there will be a multiple window event listener bug
+        // more info here: https://stackoverflow.com/questions/10444077/javascript-removeeventlistener-not-working 
+        this.inputHandler = this.inputHandler.bind(this); 
     }
-    drawTextBox(){
+    drawTextBox(){ 
         const lines = this.text.split("\n");
         this.h = lines.length * getFontDimensions(this.ctx, lines[0])[1]; 
         let maxLine = 0;
@@ -21,6 +26,45 @@ class TextBox extends Box{
         this.w = maxLine;
         this.ctx.strokeRect(this.x, this.y, this.w, this.h); 
     }
-    
+    redrawTextBox(){
+        this.clearBox(2, 2); 
+        this.drawTextBox();
+    }
+    // will trigger an edit or stop edit functionality 
+    toggleEditing(){
+        this.editing = !this.editing; 
+        if(this.editing){
+            this.onEdit(); 
+        }else{
+            this.onStopEdit();
+            console.log(window.onkeydown)  
+        }
+    }
+    onEdit(){
+        this.text += "|";
+        this.redrawTextBox();
+        window.addEventListener("keydown", this.inputHandler);  
+    }
+    onStopEdit(){
+        this.text = this.text.replace("|", ""); 
+        this.redrawTextBox(); 
+        window.removeEventListener("keydown", this.inputHandler);    
+    }
+    inputHandler(e){
+        this.text = this.text.replace("|", ""); 
+        if(e.key === "Backspace"){
+            this.text = this.text.slice(0, this.text.length -1); 
+        }
+        if(e.key === "Enter"){
+            this.text += "\n"; 
+        }
+        if(e.key.length === 1){
+            this.text +=  e.key; 
+        }
+        this.text += "|";
+        this.redrawTextBox(); 
+    }
+
+
 }
 export {TextBox}; 
